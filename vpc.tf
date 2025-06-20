4995a56100b1437a5dfec736c4733dad58708e8d
Vishnu KR <vishnukr1995@gmail.com>
2025-06-21 03:40:12 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "vkr-eks-vpc"
-  cidr = var.vps_cidr
+  cidr = var.vpc_cidr
 
   azs             = data.aws_availability_zones.available.names
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
